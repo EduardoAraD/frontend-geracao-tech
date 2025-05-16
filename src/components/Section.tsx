80a0582c00137a1ae873e0e59b0a1687f67884a2
eduardoarad <araujocarlos893@gmail.com>
2025-05-16 14:05:52 -0300

    finish home Mobile

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,11 +2,12 @@ import type { ReactNode } from "react";
 
 interface SectionProps {
   children: ReactNode
+  bgColor?: string
 }
 
-const Section = ({ children }: SectionProps) => {
+const Section = ({ children, bgColor = '' }: SectionProps) => {
   return (
-    <section className="flex flex-col px-5 py-10 gap-2.5">
+    <section className={`flex flex-col px-5 py-10 gap-2.5 ${bgColor}`}>
       {children}
     </section>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button";
 import Carrousel from "../components/Carousel";
 import CollectionCard from "../components/CollectionCard";
 import ProductCard from "../components/ProductCard";
@@ -47,8 +48,24 @@ const HomePage = () => {
             <ProductCard {...item} />
           ))}
         </div>
+      </Section>
 
-        
+      <Section bgColor="bg-white">
+        <div className="flex justify-center items-center h-[280px] w-[280px] m-auto rounded-full bg-[linear-gradient(180deg,_#B5B6F2_0%,_#FFFFFF_60%)]">
+          <img src="/sale.png" height={280} width={280} alt="" />
+        </div>
+        <div className="flex flex-col gap-2.5">
+          <span className="text-sm font-bold text-warning">Oferta especial</span>
+          <h3 className="text-3xl font-bold text-dark_gray2">
+            Air Jordan edição de colecionador
+          </h3>
+          <p className="text-sm font-medium text-dark_gray2 mt-2.5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident enim at quas laudantium. Fuga necessitatibus sint quia nulla? Aliquam, illum suscipit reprehenderit soluta iusto quia quos corporis dicta voluptas eaque.
+          </p>
+          <div className="mt-5 w-52">
+            <Button>Ver Oferta</Button>
+          </div>
+        </div>
       </Section>
     </main>
   );
